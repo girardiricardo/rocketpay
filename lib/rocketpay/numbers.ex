a902473ef6cba4836842e7be7346cddb2e3bacba
Ricardo Girardi <girardiricardo@outlook.com>
2021-03-29 13:50:48 -0300

    🚥  working with pipes

--- lib/rocketpay/numbers.ex
+++ lib/rocketpay/numbers.ex
@@ -1,9 +1,19 @@
 defmodule Rocketpay.Numbers do
   def sum_from_file(filename) do
-    file = File.read("#{filename}.csv")
-    handle_file(file)
+    "#{filename}.csv"
+    |> File.read()
+    |> handle_file()
   end
 
-  defp handle_file({:ok, file}), do: file
+  defp handle_file({:ok, result}) do
+    result =
+      result
+      |> String.split(",")
+      |> Enum.map(fn number -> String.to_integer(number) end)
+      |> Enum.sum()
+
+    {:ok, %{result: result}}
+  end
+
   defp handle_file({:error, _reason}), do: {:error, "Invalid file"}
 end
