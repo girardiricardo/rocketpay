bad9736b1741a91cc0382ba39ee5214101a44750
Ricardo Girardi <girardiricardo@outlook.com>
2021-03-29 14:15:58 -0300

    📝 working with params and status requets

--- lib/rocketpay/numbers.ex
+++ lib/rocketpay/numbers.ex
@@ -15,5 +15,5 @@ defmodule Rocketpay.Numbers do
     {:ok, %{result: result}}
   end
 
-  defp handle_file({:error, _reason}), do: {:error, "Invalid file"}
+  defp handle_file({:error, _reason}), do: {:error, %{message: "Inválid file!"}}
 end
